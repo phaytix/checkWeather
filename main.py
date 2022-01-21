2ce9349e0749363dc52349e59dcf5c6b090af949
phaytix <tshurts@protonmail.com>
2022-01-20 18:32:58 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# https://openweathermap.org/ THE LINK FOR THE WEBSITE! You do have to create an account but its free
+
+import requests
+
+API_KEY = "58d21f998621e95d8dc74b1740c0b121"
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+
+city = input("Enter a city name: ")
+request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
+response = requests.get(request_url)
+
+if response.status_code == 200:
+    data = response.json()
+    weather = data['weather'][0]['description']
+    temperature = round(data["main"]["temp"] - 273.15, 2)
+
+    print("Weather:", weather)
+    print("Temperature:", temperature, "celsius")
+else:
+    print("An error occurred.")
